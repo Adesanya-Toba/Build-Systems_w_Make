f9d85491c1f8bc2e73493b93edb2d39ee51c4e27
Toba-Adesanya <adesanyatoba@gmail.com>
2022-09-13 09:28:05 +0100

    Update workflow yml

---
+++ 3-2_Make/3-2-1_mk_wildcards/mutlifile_func.h
@@ -0,0 +1,7 @@
+#ifndef _3_2_1_MK_WILDCARDS_MUTLIFILE_FUNC_H_
+#define _3_2_1_MK_WILDCARDS_MUTLIFILE_FUNC_H_
+
+void say_hello(void);
+void print_the_time(void);
+
+#endif // _3_2_1_MK_WILDCARDS_MUTLIFILE_FUNC_H_
